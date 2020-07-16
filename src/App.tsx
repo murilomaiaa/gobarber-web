cfa16312e0cee9711777d32d024b5a58e51429f1
Murilo Maia <murilomaia.bb@gmail.com>
2020-07-16 01:46:29 -0300

    creating blobal style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const App: React.FunctionComponent = () => <h1>Hello World</h1>;
+import GlobalStyle from './styles/global';
+
+const App: React.FunctionComponent = () => (
+  <>
+    <h1>Hello World</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
