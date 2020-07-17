94fbf696a759c78f3c0f4c17c1342166cd6d9f27
Murilo Maia <murilomaia.bb@gmail.com>
2020-07-17 01:28:13 -0300

    chore/using-unform

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 
 import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
+import { Form } from '@unform/web';
+
 import { Container, Content, Background } from './styles';
 import logoImage from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
   return (
     <Container>
       <Background />
       <Content>
         <img src={logoImage} alt="Go Barber" />
 
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
           <Input
@@ -31,7 +37,7 @@ const SignUp: React.FC = () => {
           />
 
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
 
         <a href="singup">
           <FiArrowLeft />
